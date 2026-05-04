5e8557dec08f7bcd9ae72529acb0e73fa2426875
Monica Gushiken <monica.gushiken@gmail.com>
2026-05-03 21:02:20 -0300

    feat: implement semantic search and RAG prompt chain

--- src/search.py
+++ src/search.py
@@ -1,3 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_postgres import PGVector
+
+load_dotenv()
+
 PROMPT_TEMPLATE = """
 CONTEXTO:
 {contexto}
@@ -25,5 +36,33 @@ PERGUNTA DO USUÁRIO:
 RESPONDA A "PERGUNTA DO USUÁRIO"
 """
 
+for k in ("GOOGLE_API_KEY", "OPENAI_API_KEY", "DATABASE_URL","PG_VECTOR_COLLECTION_NAME", "PDF_PATH"):
+    if not os.getenv(k):
+        raise RuntimeError(f"Environment variable {k} is not set")
+
+# OpenAI embeddings
+embeddings = OpenAIEmbeddings(model=os.getenv("OPENAI_EMBEDDING_MODEL", "text-embedding-3-small"))
+
+# Google embeddings
+# embeddings = GoogleGenerativeAIEmbeddings(model=os.getenv("GOOGLE_EMBEDDING_MODEL", "models/embedding-001"))
+
+store = PGVector(
+    embeddings = embeddings,
+    collection_name = os.getenv("PG_VECTOR_COLLECTION_NAME"),
+    connection = os.getenv("DATABASE_URL"),
+    use_jsonb = True
+)
+
+prompt = PromptTemplate(
+    template=PROMPT_TEMPLATE,
+    input_variables=["contexto", "pergunta"]
+)
+
+model = ChatOpenAI(model="gpt-5-nano", temperature=0)
+
+chain = prompt | model
+
 def search_prompt(question=None):
-    pass
+    results = store.similarity_search(query=question, k=10)
+    
+    return chain.invoke({"contexto": "\n\n".join([r.page_content for r in results]), "pergunta": question}).content
